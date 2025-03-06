43f3aced0741134f2937ac253fdf6f553df86b3f
Chris Jackson <cej29@students.uwf.edu>
2025-03-06 14:51:47 -0600

    deleted files from merging

--- routes/predictionRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const{ predictHeartDisease } = require('../controllers/predictionControllers')
-
-const router = express.Router()
-
-router.post('/', predictHeartDisease) // POST request for predictions
-
-module.exports = router
